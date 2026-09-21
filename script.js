cdc52e04616cedadc0c05a13f1ab74827da6ac13
brunohsantana-dev <brunohsantana.dev@gmail.com>
2026-09-21 12:29:11 -0300

    jokenpo is alive results and score running

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ Precisamos mostrar o resultado na tela e mostrar o placar com pontos atualizados
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
+const playerChoiceDisplay = document.getElementById("player-choice")
+const computerChoiceDisplay = document.getElementById("computer-choice")
+const resultMessage = document.getElementById("result-message")
+const playerScoreDisplay = document.getElementById("player-score")
+const computerScoreDisplay = document.getElementById("computer-score")
+
+let playerScore = 0
+let computerScore = 0
+
+
 
 const choices = ["rock", "paper", "scissors"]
 
@@ -19,21 +29,38 @@ const pickAHand = () => {
 const play = (playerChoice) => {
     const computerChoice = pickAHand()
 
-    console.log(playerChoice, computerChoice)
-    
+    playerChoiceDisplay.textContent = playerChoice
+    computerChoiceDisplay.textContent = computerChoice
+
+
 
     if (playerChoice === computerChoice) {
-        console.log("DRAW")
+        resultMessage.textContent = "DRAW"
+
+
     } else if (
         (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "scissors" && computerChoice === "paper") ||
         (playerChoice === "paper" && computerChoice === "rock")
     ) {
-        console.log("YOU WIN")
+        resultMessage.textContent = "YOU WIN"
+        playerScore++
+
     } else {
-        console.log("COMPUTER WINS")
+        resultMessage.textContent = "COMPUTER WINS"
+        computerScore++
+
     }
+
+console.log("Player score:", playerScore)
+console.log("Computer score:", computerScore)
+
+playerScoreDisplay.textContent = playerScore
+computerScoreDisplay.textContent = computerScore
+
 }
+
+
 
 
 rock.addEventListener("click", () => {
@@ -47,6 +74,11 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
     play("scissors")
 })
+
+
+
+
+
 
 
 
